4ded4556d667c8d636c309f226d5a3471902da45
Mark <voronkov1mark@gmail.com>
2023-03-09 23:52:34 +0300

    сложно

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,109 @@
 import org.w3c.dom.ls.LSOutput;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        // задача 1
-        int[] num = new int[3];
-        num[0] = 1;
-        num[1] = 2;
-        num[2] = 3;
-        double[] numWithComa = {1.57, 7.654, 9.986};
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+
+    private static void task4() {
+        System.out.println("Задача 4");
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                System.out.print(numbers[i] +", ");
+            } else {
+                System.out.print(numbers[i]+1 +", ");
+            }
+        }
+    }
+
+
+    private static void task3() {
+        System.out.println("Задача 3");
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            if (numbers[i] <= numbers.length) {
+                System.out.print(numbers[i] + ", ");
+            } else {
+                System.out.print(numbers[i]);
+            }
+        }
+        System.out.println();
+        double[] numbersWithComa = {1.57, 7.654, 9.986};
+        for (int i = numbersWithComa.length - 1; i >= 0; i--) {
+            if (i > 0) {
+                System.out.print(numbersWithComa[i] + ", ");
+            } else {
+                System.out.print(numbersWithComa[i]);
+            }
+
+        }
+        System.out.println();
         int[] seasons = {1, 2, 3, 4};
-        // задача 2
-        System.out.println(num[0] + ", " + num[1] + ", " + num[2]);
-        System.out.println(numWithComa[0] + ", " + numWithComa[1] + ", " + numWithComa[2]);
-        System.out.println(seasons[0] + ", " + seasons[1] + ", " + seasons[2] + ", " + seasons[3]);
+        for (int i = seasons.length - 1; i >= 0; i--) {
+            if (seasons[i] - 1 <= 0) {
+                System.out.print(seasons[i]);
+            } else {
+                System.out.print(seasons[i] + ", ");
+            }
+
+        }
         System.out.println();
-        // задача 3
-        System.out.println(num[2] + ", " + num[1] + ", " + num[0]);
-        System.out.println(numWithComa[2] + ", " + numWithComa[1] + ", " + numWithComa[0]);
-        System.out.println(seasons[3] + ", " + seasons[2] + ", " + seasons[1] + ", " + seasons[0]);
-        System.out.println();
-        // задача 4
+    }
 
 
+    private static void task2() {
+        System.out.println("Задача 2");
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] != numbers.length) {
+                System.out.print(numbers[i] + ", ");
+            } else {
+                System.out.print(numbers[i]);
+            }
+        }
+        System.out.println();
+        double[] numbersWithComa = {1.57, 7.654, 9.986};
+        for (int i = 0; i < numbersWithComa.length; i++) {
+            if (i + 1 != numbersWithComa.length) {
+                System.out.print(numbersWithComa[i] + ", ");
+            } else {
+                System.out.print(numbersWithComa[i]);
+            }
+
+        }
+        System.out.println();
+        int[] seasons = {1, 2, 3, 4};
+        for (int i = 0; i < seasons.length; i++) {
+            if (i + 1 != seasons.length) {
+                System.out.print(seasons[i] + ", ");
+            } else {
+                System.out.print(seasons[i]);
+            }
+
+        }
+        System.out.println();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        double[] numbersWithComa = {1.57, 7.654, 9.986};
+        int[] seasons = {1, 2, 3, 4};
     }
 }
