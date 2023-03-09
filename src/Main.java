3030b6ce3041b7d7e2a616994d1649f03f5fe0d8
Mark <voronkov1mark@gmail.com>
2023-03-09 15:50:12 +0300

    без циклов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // задача 1
+        int[] num = new int[3];
+        num[0] = 1;
+        num[1] = 2;
+        num[2] = 3;
+        double[] numWithComa = {1.57, 7.654, 9.986};
+        int[] seasons = {1, 2, 3, 4};
+        // задача 2
+        System.out.println(num[0] + ", " + num[1] + ", " + num[2]);
+        System.out.println(numWithComa[0] + ", " + numWithComa[1] + ", " + numWithComa[2]);
+        System.out.println(seasons[0] + ", " + seasons[1] + ", " + seasons[2] + ", " + seasons[3]);
+        System.out.println();
+        // задача 3
+        System.out.println(num[2] + ", " + num[1] + ", " + num[0]);
+        System.out.println(numWithComa[2] + ", " + numWithComa[1] + ", " + numWithComa[0]);
+        System.out.println(seasons[3] + ", " + seasons[2] + ", " + seasons[1] + ", " + seasons[0]);
+        System.out.println();
+        // задача 4
+
+
     }
 }
